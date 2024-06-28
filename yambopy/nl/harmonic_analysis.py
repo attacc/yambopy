81d8feef0e1ebbd9375b347ca4587e6718f732a3
Claudio Attaccalite <claudio.attaccalite@gmail.com>
2024-06-28 10:00:04 +0200

    Fixed project

--- yambopy/nl/harmonic_analysis.py
+++ yambopy/nl/harmonic_analysis.py
@@ -7,6 +7,7 @@
 import numpy as np
 from yambopy.units import ha2ev,fs2aut, SVCMm12VMm1,AU2VMm1
 from yambopy.nl.external_efield import Divide_by_the_Field
+from tqdm import tqdm
 import scipy.linalg
 import sys
 import os
@@ -37,11 +38,16 @@ def Coefficents_Inversion(NW,NX,P,W,T_period,T_range,T_step,efield,INV_MODE):
     P_i    = np.zeros(M_size, dtype=np.double)
     T_i    = np.zeros(M_size, dtype=np.double)
     X_here = np.zeros(nP_components, dtype=np.cdouble)
+    Sampling = np.zeros((M_size,2), dtype=np.double)
+
 
 # Calculation of  T_i and P_i
     for i_t in range(M_size):
         T_i[i_t] = (i_t_start + i_deltaT * i_t)*T_step - efield["initial_time"]
         P_i[i_t] = P[i_t_start + i_deltaT * i_t]
+
+    Sampling[:,0]=T_i/fs2aut
+    Sampling[:,1]=P_i
 
 # Build the M matrix
     for i_t in range(M_size):
@@ -81,7 +87,7 @@ def Coefficents_Inversion(NW,NX,P,W,T_period,T_range,T_step,efield,INV_MODE):
         for i_t in range(M_size):
            X_here[i_n]=X_here[i_n]+INV[i_n,i_t]*P_i[i_t] 
 
-    return X_here
+    return X_here,Sampling
 
 
 
@@ -136,40 +142,16 @@ def Harmonic_Analysis(nldb, X_order=4, T_range=[-1, -1],prn_Peff=False,INV_MODE=
 
     print("Time range : ",str(T_range[0]/fs2aut),'-',str(T_range[1]/fs2aut),'[fs]')
         
+    M_size = 2*X_order + 1  # Positive and negative components plut the zero
     X_effective       =np.zeros((X_order+1,n_runs,3),dtype=np.cdouble)
     Susceptibility    =np.zeros((X_order+1,n_runs,3),dtype=np.cdouble)
+    Sampling          =np.zeros((M_size, 2,n_runs,3),dtype=np.double)    
     Harmonic_Frequency=np.zeros((X_order+1,n_runs),dtype=np.double)
 
     # Generate multiples of each frequency
     for i_order in range(X_order+1):
         Harmonic_Frequency[i_order,:]=i_order*freqs[:]
     
-
-    # Find the Fourier coefficients by inversion
-    for i_f in range(n_runs):
-        #
-        # T_period change with the laser frequency 
-        #
-        T_period=2.0*np.pi/Harmonic_Frequency[1,i_f]
-        T_range,T_range_out_of_bounds=update_T_range(T_period,T_range_initial,time)
-        #
-        if T_range_out_of_bounds:
-            print("WARNING! Time range out of bounds for frequency :",Harmonic_Frequency[1,i_f]*ha2ev,"[eV]")
-        #
-        for i_d in range(3):
-            X_effective[:,i_f,i_d]=Coefficents_Inversion(X_order+1, X_order+1, polarization[i_f][i_d,:],Harmonic_Frequency[:,i_f],T_period,T_range,T_step,efield,INV_MODE)
-
-    # Calculate Susceptibilities from X_effective
-    for i_order in range(X_order+1):
-        for i_f in range(n_runs):
-            if i_order==1:
-                Susceptibility[i_order,i_f,0]   =4.0*np.pi*np.dot(efield['versor'][:],X_effective[i_order,i_f,:])
-                Susceptibility[i_order,i_f,1:2] =0.0
-            else:
-                Susceptibility[i_order,i_f,:]=X_effective[i_order,i_f,:]
-            
-            Susceptibility[i_order,i_f,:]*=Divide_by_the_Field(nldb.Efield[i_f],i_order)
-
     loop_on_angles=False
     loop_on_frequencies=False
 
@@ -184,10 +166,38 @@ def Harmonic_Analysis(nldb, X_order=4, T_range=[-1, -1],prn_Peff=False,INV_MODE=
         loop_on_frequencies=True
         print("Loop on frequencies ...")
 
+    # Find the Fourier coefficients by inversion
+    for i_f in tqdm(range(n_runs)):
+        #
+        # T_period change with the laser frequency 
+        #
+        T_period=2.0*np.pi/Harmonic_Frequency[1,i_f]
+        T_range,T_range_out_of_bounds=update_T_range(T_period,T_range_initial,time)
+        #
+        if T_range_out_of_bounds:
+            print("WARNING! Time range out of bounds for frequency :",Harmonic_Frequency[1,i_f]*ha2ev,"[eV]")
+        #
+        for i_d in range(3):
+            X_effective[:,i_f,i_d],Sampling[:,:,i_f,i_d]=Coefficents_Inversion(X_order+1, X_order+1, polarization[i_f][i_d,:],Harmonic_Frequency[:,i_f],T_period,T_range,T_step,efield,INV_MODE)
+
+    # Calculate Susceptibilities from X_effective
+    for i_order in range(X_order+1):
+        for i_f in range(n_runs):
+            if i_order==1:
+                Susceptibility[i_order,i_f,0]   =4.0*np.pi*np.dot(efield['versor'][:],X_effective[i_order,i_f,:])
+                Susceptibility[i_order,i_f,1:2] =0.0
+            else:
+                Susceptibility[i_order,i_f,:]=X_effective[i_order,i_f,:]
+            
+            Susceptibility[i_order,i_f,:]*=Divide_by_the_Field(nldb.Efield[i_f],i_order)
+
+
+
     #Rectronstruct Polarization from the X_effective
     if(prn_Peff):
+        print("Reconstruct effective polarizations ...")
         Peff=np.zeros((n_runs,3,len(time)),dtype=np.cdouble)
-        for i_f in range(n_runs):
+        for i_f in tqdm(range(n_runs)):
             for i_d in range(3):
                 for i_order in range(X_order+1):
                     Peff[i_f,i_d,:]+=X_effective[i_order,i_f,i_d]*np.exp(-1j*i_order*freqs[i_f]*time[:])
@@ -198,7 +208,8 @@ def Harmonic_Analysis(nldb, X_order=4, T_range=[-1, -1],prn_Peff=False,INV_MODE=
         header2+="Py     "
         header2+="Pz     "
         footer2='Time dependent polarization reconstructed from Fourier coefficients'
-        for i_f in range(n_runs):
+        print("Print effective polarizations ...")
+        for i_f in tqdm(range(n_runs)):
             values2=np.c_[time.real/fs2aut]
             values2=np.append(values2,np.c_[Peff[i_f,0,:].real],axis=1)
             values2=np.append(values2,np.c_[Peff[i_f,1,:].real],axis=1)
@@ -206,8 +217,19 @@ def Harmonic_Analysis(nldb, X_order=4, T_range=[-1, -1],prn_Peff=False,INV_MODE=
             output_file2='o.YamboPy-pol_reconstructed_F'+str(i_f+1)
             np.savetxt(output_file2,values2,header=header2,delimiter=' ',footer=footer2)
 
+        # Print Sampling point
+        footer2='Sampled polarization'
+        print("Print sampled polarization ...")
+        for i_f in tqdm(range(n_runs)):
+            values=np.c_[Sampling[:,0,i_f,0]]
+            values=np.append(values,np.c_[Sampling[:,1,i_f,0]],axis=1)
+            values=np.append(values,np.c_[Sampling[:,1,i_f,1]],axis=1)
+            values=np.append(values,np.c_[Sampling[:,1,i_f,2]],axis=1)
+            output_file3='o.YamboPy-sampling_F'+str(i_f+1)
+            np.savetxt(output_file3,values,header=header2,delimiter=' ',footer=footer2)
 
     # Print the result
+    print("Write final results: xhi^1,xhi^2,xhi^3, etc...")
     for i_order in range(X_order+1):
 
         if i_order==0: 
