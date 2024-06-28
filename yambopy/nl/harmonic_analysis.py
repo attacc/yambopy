Calculate current reponse (units to be fixed)

yambopy/nl/harmonic_analysis.py
@@ -98,7 +98,7 @@ def Harmonic_Analysis(nldb, X_order=4, T_range=[-1, -1],prn_Peff=False,INV_MODE=
     n_runs=len(nldb.Polarization)
     # Array of polarizations for each laser frequency
     polarization=nldb.Polarization
-    current     =nldb.Polarization
+    current     =nldb.Current
     freqs=np.zeros(n_runs,dtype=np.double)
 
     print("\n* * * Harmonic analysis * * *\n")
@@ -142,11 +142,12 @@ def Harmonic_Analysis(nldb, X_order=4, T_range=[-1, -1],prn_Peff=False,INV_MODE=
     M_size = 2*X_order + 1  # Positive and negative components plut the zero
     X_effective       =np.zeros((X_order+1,n_runs,3),dtype=np.cdouble)
     Susceptibility    =np.zeros((X_order+1,n_runs,3),dtype=np.cdouble)
-    Sampling          =np.zeros((M_size, 2,n_runs,3),dtype=np.double)    
+    SamplingP          =np.zeros((M_size, 2,n_runs,3),dtype=np.double)    
     Harmonic_Frequency=np.zeros((X_order+1,n_runs),dtype=np.double)
     # Calculate also the current response
     Sigma_effective       =np.zeros((X_order+1,n_runs,3),dtype=np.cdouble)
     Conducibility         =np.zeros((X_order+1,n_runs,3),dtype=np.cdouble)
+    SamplingJ             =np.zeros((M_size, 2,n_runs,3),dtype=np.double)    
 
     # Generate multiples of each frequency
     for i_order in range(X_order+1):
@@ -178,9 +179,9 @@ def Harmonic_Analysis(nldb, X_order=4, T_range=[-1, -1],prn_Peff=False,INV_MODE=
             print("WARNING! Time range out of bounds for frequency :",Harmonic_Frequency[1,i_f]*ha2ev,"[eV]")
         #
         for i_d in range(3):
-            X_effective[:,i_f,i_d],Sampling[:,:,i_f,i_d]=Coefficents_Inversion(X_order+1, X_order+1, polarization[i_f][i_d,:],Harmonic_Frequency[:,i_f],T_period,T_range,T_step,efield,INV_MODE)
+            X_effective[:,i_f,i_d],SamplingP[:,:,i_f,i_d]=Coefficents_Inversion(X_order+1, X_order+1, polarization[i_f][i_d,:],Harmonic_Frequency[:,i_f],T_period,T_range,T_step,efield,INV_MODE)
         for i_d in range(3):
-            Sigma_effective[:,i_f,i_d],Sampling[:,:,i_f,i_d]=Coefficents_Inversion(X_order+1, X_order+1, current[i_f][i_d,:],Harmonic_Frequency[:,i_f],T_period,T_range,T_step,efield,INV_MODE)
+            Sigma_effective[:,i_f,i_d],SamplingJ[:,:,i_f,i_d]=Coefficents_Inversion(X_order+1, X_order+1, current[i_f][i_d,:],Harmonic_Frequency[:,i_f],T_period,T_range,T_step,efield,INV_MODE)
 
     # Calculate Susceptibilities from X_effective
     for i_order in range(X_order+1):
@@ -211,7 +212,7 @@ def Harmonic_Analysis(nldb, X_order=4, T_range=[-1, -1],prn_Peff=False,INV_MODE=
                     Peff[i_f,i_d,:]+=np.conj(X_effective[i_order,i_f,i_d])*np.exp(+1j*i_order*freqs[i_f]*time[:])
                     Jeff[i_f,i_d,:]+=Sigma_effective[i_order,i_f,i_d]*np.exp(-1j*i_order*freqs[i_f]*time[:])
                     Jeff[i_f,i_d,:]+=np.conj(Sigma_effective[i_order,i_f,i_d])*np.exp(+1j*i_order*freqs[i_f]*time[:])
-        # Print reconstructed polarization/current
+       # Print reconstructed polarization/current
 
         headerP="[fs]            "
         headerP+="Px     "
@@ -219,30 +220,51 @@ def Harmonic_Analysis(nldb, X_order=4, T_range=[-1, -1],prn_Peff=False,INV_MODE=
         headerP+="Pz     "
         footerP='Time dependent polarization reconstructed from Fourier coefficients'
 
+        headerJ="[fs]            "
+        headerJ+="Jx     "
+        headerJ+="Jy     "
+        headerJ+="Jz     "
+        footerJ='Time dependent current reconstructed from Fourier coefficients'
 
-
-        print("Print effective polarizations ...")
+        print("Print effective polarizations/currents ...")
         for i_f in tqdm(range(n_runs)):
-            values2=np.c_[time.real/fs2aut]
-            values2=np.append(values2,np.c_[Peff[i_f,0,:].real],axis=1)
-            values2=np.append(values2,np.c_[Peff[i_f,1,:].real],axis=1)
-            values2=np.append(values2,np.c_[Peff[i_f,2,:].real],axis=1)
-            output_file2='o.YamboPy-pol_reconstructed_F'+str(i_f+1)
-            np.savetxt(output_file2,values2,header=headerP,delimiter=' ',footer=footer2)
+            valuesP=np.c_[time.real/fs2aut]
+            valuesP=np.append(valuesP,np.c_[Peff[i_f,0,:].real],axis=1)
+            valuesP=np.append(valuesP,np.c_[Peff[i_f,1,:].real],axis=1)
+            valuesP=np.append(valuesP,np.c_[Peff[i_f,2,:].real],axis=1)
+            output_fileP='o.YamboPy-pol_reconstructed_F'+str(i_f+1)
+
+            valuesJ=np.c_[time.real/fs2aut]
+            valuesJ=np.append(valuesJ,np.c_[Jeff[i_f,0,:].real],axis=1)
+            valuesJ=np.append(valuesJ,np.c_[Jeff[i_f,1,:].real],axis=1)
+            valuesJ=np.append(valuesJ,np.c_[Jeff[i_f,2,:].real],axis=1)
+            output_fileJ='o.YamboPy-curr_reconstructed_F'+str(i_f+1)
+
+            np.savetxt(output_fileP,valuesP,header=headerP,delimiter=' ',footer=footerP)
+            np.savetxt(output_fileJ,valuesJ,header=headerJ,delimiter=' ',footer=footerJ)
 
         # Print Sampling point
-        footer2='Sampled polarization'
-        print("Print sampled polarization ...")
+        footerP='Sampled polarization'
+        footerJ='Sampled current'
+        print("Print sampled polarization/current ...")
         for i_f in tqdm(range(n_runs)):
-            values=np.c_[Sampling[:,0,i_f,0]]
-            values=np.append(values,np.c_[Sampling[:,1,i_f,0]],axis=1)
-            values=np.append(values,np.c_[Sampling[:,1,i_f,1]],axis=1)
-            values=np.append(values,np.c_[Sampling[:,1,i_f,2]],axis=1)
-            output_file3='o.YamboPy-sampling_F'+str(i_f+1)
-            np.savetxt(output_file3,values,header=header2,delimiter=' ',footer=footer2)
+            valuesP=np.c_[SamplingP[:,0,i_f,0]]
+            valuesP=np.append(valuesP,np.c_[SamplingP[:,1,i_f,0]],axis=1)
+            valuesP=np.append(valuesP,np.c_[SamplingP[:,1,i_f,1]],axis=1)
+            valuesP=np.append(valuesP,np.c_[SamplingP[:,1,i_f,2]],axis=1)
+            output_fileP='o.YamboPy-sampling_pol_F'+str(i_f+1)
+            np.savetxt(output_fileP,valuesP,header=headerP,delimiter=' ',footer=footerP)
+
+            valuesJ=np.c_[SamplingJ[:,0,i_f,0]]
+            valuesJ=np.append(valuesJ,np.c_[SamplingJ[:,1,i_f,0]],axis=1)
+            valuesJ=np.append(valuesJ,np.c_[SamplingJ[:,1,i_f,1]],axis=1)
+            valuesJ=np.append(valuesJ,np.c_[SamplingJ[:,1,i_f,2]],axis=1)
+            output_fileJ='o.YamboPy-sampling_curr_F'+str(i_f+1)
+            np.savetxt(output_fileJ,valuesJ,header=headerJ,delimiter=' ',footer=footerJ)
+
 
     # Print the result
-    print("Write final results: xhi^1,xhi^2,xhi^3, etc...")
+    print("Write final results: xhi^1,xhi^2,xhi^3,... and sigma^1,sigma^2,sigma^3,....")
     for i_order in range(X_order+1):
 
         if i_order==0: 
@@ -251,8 +273,10 @@ def Harmonic_Analysis(nldb, X_order=4, T_range=[-1, -1],prn_Peff=False,INV_MODE=
             Unit_of_Measure = np.power(SVCMm12VMm1/AU2VMm1,i_order-1,dtype=np.double)
         
         Susceptibility[i_order,:,:]=Susceptibility[i_order,:,:]*Unit_of_Measure
+        Conducibility[i_order,:,:] =Conducibility[i_order,:,:]*Unit_of_Measure
 
-        output_file='o.YamboPy-X_probe_order_'+str(i_order)
+        output_fileP='o.YamboPy-X_probe_order_'+str(i_order)
+        output_fileJ='o.YamboPy-Sigma_probe_order_'+str(i_order)
 
         if loop_on_angles:
 
@@ -261,30 +285,49 @@ def Harmonic_Analysis(nldb, X_order=4, T_range=[-1, -1],prn_Peff=False,INV_MODE=
             header0="[eV]           "
 
         if i_order == 0 or i_order ==1:
-            header =header0
-            header+="X/Im(x)            X/Re(x)            X/Im(y)            X/Re(y)            X/Im(z)            X/Re(z)"
+            headerP =header0
+            headerP+="X/Im(x)            X/Re(x)            X/Im(y)            X/Re(y)            X/Im(z)            X/Re(z)"
+            headerJ =header0
+            headerJ+="Sigma/Im(x)            Sigma/Re(x)            Sigma/Im(y)            Sigma/Re(y)            Sigma/Im(z)            Sigma/Re(z)"
         else:
-            header=header0
-            header+="X/Im[cm/stV]^%d     X/Re[cm/stV]^%d     " % (i_order-1,i_order-1)
-            header+="X/Im[cm/stV]^%d     X/Re[cm/stV]^%d     " % (i_order-1,i_order-1)
-            header+="X/Im[cm/stV]^%d     X/Re[cm/stV]^%d     " % (i_order-1,i_order-1)
+            headerP=header0
+            headerP+="X/Im[cm/stV]^%d     X/Re[cm/stV]^%d     " % (i_order-1,i_order-1)
+            headerP+="X/Im[cm/stV]^%d     X/Re[cm/stV]^%d     " % (i_order-1,i_order-1)
+            headerP+="X/Im[cm/stV]^%d     X/Re[cm/stV]^%d     " % (i_order-1,i_order-1)
+            headerJ=header0
+            headerJ+="Sigma/Im[cm/stV]^%d     Sigma/Re[cm/stV]^%d     " % (i_order-1,i_order-1)
+            headerJ+="Sigma/Im[cm/stV]^%d     Sigma/Re[cm/stV]^%d     " % (i_order-1,i_order-1)
+            headerJ+="Sigma/Im[cm/stV]^%d     Sigma/Re[cm/stV]^%d     " % (i_order-1,i_order-1)
 
         if loop_on_frequencies:
-            values=np.c_[freqs*ha2ev]
+            valuesP=np.c_[freqs*ha2ev]
+            valuesJ=np.c_[freqs*ha2ev]
         elif loop_on_angles:
-            values=np.c_[angles]
-        values=np.append(values,np.c_[Susceptibility[i_order,:,0].imag],axis=1)
-        values=np.append(values,np.c_[Susceptibility[i_order,:,0].real],axis=1)
-        values=np.append(values,np.c_[Susceptibility[i_order,:,1].imag],axis=1)
-        values=np.append(values,np.c_[Susceptibility[i_order,:,1].real],axis=1)
-        values=np.append(values,np.c_[Susceptibility[i_order,:,2].imag],axis=1)
-        values=np.append(values,np.c_[Susceptibility[i_order,:,2].real],axis=1)
+            valuesP=np.c_[angles]
+            valuesJ=np.c_[angles]
+        valuesP=np.append(valuesP,np.c_[Susceptibility[i_order,:,0].imag],axis=1)
+        valuesP=np.append(valuesP,np.c_[Susceptibility[i_order,:,0].real],axis=1)
+        valuesP=np.append(valuesP,np.c_[Susceptibility[i_order,:,1].imag],axis=1)
+        valuesP=np.append(valuesP,np.c_[Susceptibility[i_order,:,1].real],axis=1)
+        valuesP=np.append(valuesP,np.c_[Susceptibility[i_order,:,2].imag],axis=1)
+        valuesP=np.append(valuesP,np.c_[Susceptibility[i_order,:,2].real],axis=1)
 
-        footer=" \n"
+        valuesJ=np.append(valuesJ,np.c_[Conducibility[i_order,:,0].imag],axis=1)
+        valuesJ=np.append(valuesJ,np.c_[Conducibility[i_order,:,0].real],axis=1)
+        valuesJ=np.append(valuesJ,np.c_[Conducibility[i_order,:,1].imag],axis=1)
+        valuesJ=np.append(valuesJ,np.c_[Conducibility[i_order,:,1].real],axis=1)
+        valuesJ=np.append(valuesJ,np.c_[Conducibility[i_order,:,2].imag],axis=1)
+        valuesJ=np.append(valuesJ,np.c_[Conducibility[i_order,:,2].real],axis=1)
+
+        footerP=" \n"
+        footerJ=" \n"
         if loop_on_angles:
-            footer+="Laser frequency : "+str(freqs[0]*ha2ev)+" [eV] \n"
-        footer+='Non-linear response analysis performed using YamboPy\n '
-        np.savetxt(output_file,values,header=header,delimiter=' ',footer=footer)
+            footerP+="Laser frequency : "+str(freqs[0]*ha2ev)+" [eV] \n"
+            footerJ+="Laser frequency : "+str(freqs[0]*ha2ev)+" [eV] \n"
+        footerP+='Non-linear response analysis performed using YamboPy\n '
+        footerJ+='Non-linear response analysis performed using YamboPy\n '
+        np.savetxt(output_fileP,valuesP,header=headerP,delimiter=' ',footer=footerP)
+        np.savetxt(output_fileJ,valuesJ,header=headerJ,delimiter=' ',footer=footerJ)
 
 def update_T_range(T_period,T_range_initial,time):
         #